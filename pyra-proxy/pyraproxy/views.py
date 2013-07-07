218768d4b04a3737f5fd1f5077339eb840ea965b
Austin Rasmussen <ar@austinrasmussen.com>
2013-07-07 06:20:58 +0400

    Add form post (not file) support

--- pyra-proxy/pyraproxy/views.py
+++ pyra-proxy/pyraproxy/views.py
@@ -16,10 +16,14 @@ class HomeView(object):
 	def mirror(self):
 		request_url = self.request.path_info[1:]
 		# Split off the protocol
-		content = requests.get('http://%s?%s' % (request_url.split(':/', 1)[-1], self.request.query_string), stream=True)
+		request_full_url = 'http://%s?%s' % (request_url.split(':/', 1)[-1], self.request.query_string)
+		
+		if self.request.method == 'GET':
+			content = requests.get(request_full_url, stream=True)
+		elif self.request.method == 'POST':
+			content = requests.post(request_full_url, stream=True, data=self.request.POST.mixed())
 		headers = content.headers
 		headers.pop('transfer-encoding', None)
 		headers.pop('content-encoding', None)
 		if headers.get('content-type', None) <> 'application/octet-stream':  headers.pop('content-length', None)
-		
 		return Response(app_iter=content.iter_content(chunk_size=512*1024), headers=headers)
